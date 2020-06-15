8b0ef45b7d66d525db43984e9484dd95ab2bb55c
Bhanu Pratap <singh.bpratap86@gmail.com>
2020-06-15 15:00:56 +0530

    Create tweet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @tweets = Tweet.followings_tweets_for(current_user)
+    @new_tweek = current_user.tweets.new
   end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,5 +5,13 @@ class TweetsController < ApplicationController
   end
 
   def create
+    current_user.tweets.create(permitted_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def permitted_params
+    params.require(:tweet).permit(:content)
   end
 end
